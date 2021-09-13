5624de8691047c778cd39ff8db2496abdb976430
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-13 19:47:38 +0530

    trip updates

---
+++ src/TripSelection.java
@@ -0,0 +1,6 @@
+public class TripSelection {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Trip updates");
+        System.out.println("We will start trip to yellowstone");
+    }
+}
